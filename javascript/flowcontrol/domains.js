e27b8091ead11d9fa60cc31ce3bea9771743d409
Jan Bodnar <jan.bodnar@gmail.com>
2018-07-24 22:12:13 +0200

    Create domains.js

---
+++ javascript/flowcontrol/domains.js
@@ -0,0 +1,19 @@
+let domain = "sk";
+
+switch (domain) {
+    case "us":
+        console.log("United States");
+    break;
+    case "de":
+        console.log("Germany");
+    break;
+    case "sk":
+        console.log("Slovakia");
+    break;
+    case "hu":
+        console.log("Hungary");
+    break;
+    default:
+        console.log("Unknown");
+    break;
+}
