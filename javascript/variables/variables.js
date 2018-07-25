d70a0ff0f09077fd46cbc139ab0af4e478f9a07a
Jan Bodnar <jan.bodnar@gmail.com>
2018-07-25 18:07:05 +0200

    Create variables.js

---
+++ javascript/variables/variables.js
@@ -0,0 +1,11 @@
+var name = 'Peter';
+console.log(name);
+
+name = 'Lucia';
+console.log(name);
+
+var x = 10, y = 20;
+console.log(`x:${x}, y:${y}`);
+
+var a = b = c = d = 11;
+console.log(`a:${a}, b:${c}, c:${c}, d:${d}`);
