0323b271b6b092d4474ef0bd6e51050f12122de7
Jan Bodnar <jan.bodnar@gmail.com>
2018-07-25 18:13:02 +0200

    Create boolean.js

---
+++ javascript/datatypes/boolean.js
@@ -0,0 +1,13 @@
+let male = false;
+
+let r = Math.round(Math.random());
+
+male = r ? true: false;
+
+console.log(male);
+
+if (male) {
+    console.log("We will use name John");
+} else {
+    console.log("We will use name Victoria");
+}
