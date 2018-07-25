1420b8e66d9d7ff5bd78cb4771dc47cfc35cff7e
Jan Bodnar <jan.bodnar@gmail.com>
2018-07-25 18:13:49 +0200

    Create bools.js

---
+++ javascript/datatypes/bools.js
@@ -0,0 +1,8 @@
+
+console.log(new Boolean());
+console.log(new Boolean(0));
+console.log(new Boolean(""));
+console.log(new Boolean("  "));
+console.log(new Boolean(false));
+console.log(new Boolean(true));
+console.log(new Boolean("string"));
