9eae4aa8c8a6be9f23522586c4d726c7e7ce9d2b
Jan Bodnar <jan.bodnar@gmail.com>
2018-07-25 18:18:30 +0200

    Create typeof.js

---
+++ javascript/datatypes/typeof.js
@@ -0,0 +1,16 @@
+
+let x = "Javascript";
+let y = 23;
+let z = 44.3;
+let v = true;
+let w = undefined;
+let s = Symbol('mysym');
+let t = null;
+
+console.log(typeof(x));
+console.log(typeof(y));
+console.log(typeof(z));
+console.log(typeof(v));
+console.log(typeof(w));
+console.log(typeof(s));
+console.log(typeof(n));
