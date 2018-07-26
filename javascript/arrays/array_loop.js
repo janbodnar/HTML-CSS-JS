00391adbcebefca7f09c2e0ee3f1aacc2ab3c84c
Jan Bodnar <jan.bodnar@gmail.com>
2018-07-26 13:38:38 +0200

    Create array_loop.js

---
+++ javascript/arrays/array_loop.js
@@ -0,0 +1,29 @@
+
+const words = ["pen", "pencil", "rock", "sky", "earth"];
+
+words.forEach(e => console.log(e));
+
+for (let word of words) {
+    
+    console.log(word);
+}
+
+for (let idx in words) {
+    
+    console.log(words[idx]);
+}
+
+const len = words.length;
+
+for (let i = 0; i < len; i++) {
+
+    console.log(words[i]);
+}
+
+const i = 0;
+
+while (i < len) {
+    
+    console.log(words[i]);
+    i++;
+}
