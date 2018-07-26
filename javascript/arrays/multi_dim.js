6806d5f8bff73f668f28ca5e08894881035e6554
Jan Bodnar <jan.bodnar@gmail.com>
2018-07-26 13:40:06 +0200

    Create multi_dim.js

---
+++ javascript/arrays/multi_dim.js
@@ -0,0 +1,9 @@
+const nums = [2, 3, 2, [33, 44, 55], [7, 8, [77, 88]]];
+
+console.log(nums[2]);
+
+console.log(nums[3]);
+console.log(nums[3][0]);
+
+console.log(nums[4][0]);
+console.log(nums[4][2][0]);
