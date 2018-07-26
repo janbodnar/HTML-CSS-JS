6a540d78496f410b0ead72ee30fd16585a9bba9b
Jan Bodnar <jan.bodnar@gmail.com>
2018-07-26 13:38:03 +0200

    Create basic_oper.js

---
+++ javascript/arrays/basic_oper.js
@@ -0,0 +1,13 @@
+const words = [];
+words.push("pen");
+words.push("pencil", "knife", "chair");
+
+console.log(words);
+
+const el1 = words.shift();
+console.log(el1);
+console.log(words);
+
+const el2 = words.pop();
+console.log(el2);
+console.log(words);
