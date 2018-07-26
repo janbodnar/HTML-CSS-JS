fea9edd084d6c6ffd7d3298445419a6d364367aa
Jan Bodnar <jan.bodnar@gmail.com>
2018-07-26 13:37:40 +0200

    Create array_index.js

---
+++ javascript/arrays/array_index.js
@@ -0,0 +1,10 @@
+const nums = [1, 2, 3, 4, 5, 1];
+
+const e1 = nums[0];
+console.log(e1);
+
+nums[2] = 22;
+console.log(nums[2]);
+
+console.log(nums.indexOf(1));
+console.log(nums.lastIndexOf(1));
