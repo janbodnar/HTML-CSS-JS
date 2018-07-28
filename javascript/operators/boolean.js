c8646321a1dd09b6343258d0a7f4f85d993085c0
Jan Bodnar <jan.bodnar@gmail.com>
2018-07-28 11:56:44 +0200

    Create boolean.js

---
+++ javascript/operators/boolean.js
@@ -0,0 +1,32 @@
+
+let a = true || true;
+let b = true || false;
+let c = false || true;
+let d = false || false;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+console.log("***********************")
+
+let x = true && true;
+let y = true && false;
+let z = false && true;
+let w = false && false;
+
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(w);
+
+console.log("***********************")
+
+let i = !true;
+let j = !false;
+let k = !(true && false);
+
+console.log(i);
+console.log(j);
+console.log(k);
