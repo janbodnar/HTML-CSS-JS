8d096fbd27fee9ab5263c114ee99cb3116dd2510
Jan Bodnar <jan.bodnar@gmail.com>
2018-07-28 11:14:35 +0200

    Create basic.js

---
+++ javascript/operators/basic.js
@@ -0,0 +1,22 @@
+
+// assignment operator
+let x = 1;
+
+// increment operator
+x++;
+x++;
+
+console.log(x);
+
+// decrement operator
+x--;
+console.log(x);
+
+// unary operators
+let y = -x;
+let z = -(-x);
+let w = +x;
+
+console.log(y);
+console.log(z);
+console.log(w);
