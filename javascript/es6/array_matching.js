2be9aeed4413ffa01e93ab61822e4024940b6551
Jan Bodnar <jan.bodnar@gmail.com>
2018-07-28 13:58:56 +0200

    Create array_matching.js

---
+++ javascript/es6/array_matching.js
@@ -0,0 +1,13 @@
+
+let x = [1, 2, 3, 4, 5]
+let [y, z] = x
+
+console.log(y); // 1
+console.log(z); // 2
+
+let nums = [1, 2, 3, 4, 5];
+
+let [a, b, c, d, e] = nums;
+
+console.log(a); // 1
+console.log(e); // 5
