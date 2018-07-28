e6118ba65f45810c738481cbf8e9692c172fc80f
Jan Bodnar <jan.bodnar@gmail.com>
2018-07-28 11:43:00 +0200

    Create arithmetic.js

---
+++ javascript/operators/arithmetic.js
@@ -0,0 +1,23 @@
+
+let a = 10;
+let b = 11;
+let c = 12;
+
+let add = a + b + c;
+let sub = c - a;
+let mult = a * b;
+let div = c / 3;
+
+console.log(add, sub, mult, div);
+
+let power = a ** 2;
+let mod = 11 % 3;
+
+console.log(power);
+console.log(mod);
+
+let x = 10;
+let y = 0;
+let z = x / y;
+
+console.log(z);
