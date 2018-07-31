72ed6ac2fd0b5115af4dc26ce37f0a98dace220a
Jan Bodnar <jan.bodnar@gmail.com>
2018-07-31 10:43:44 +0200

    Update read_file.js

--- javascript/nodejs/read_file.js
+++ javascript/nodejs/read_file.js
@@ -1,8 +1,8 @@
-const fs = require('fs')
+const fs = require('fs');
 
 fs.readFile('words.txt', 'utf-8', (err, data) => {
     if (err) throw err;
     console.log(data);
 });
 
-console.log("Script continues...")
+console.log("Script continues...");
