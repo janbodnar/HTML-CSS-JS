07a0ef078846e347e19e4c334747b6a682229c3f
Jan Bodnar <jan.bodnar@gmail.com>
2018-07-31 19:16:22 +0200

    Create insert_after.js

---
+++ javascript/libs/cheerio/insert_after.js
@@ -0,0 +1,16 @@
+const cheerio = require('cheerio');
+const request = require('request');
+
+request({
+    method: 'GET',
+    url: 'http://localhost:8000'
+}, (err, res, body) => {
+
+    if (err) return console.error(err);
+
+    let $ = cheerio.load(body);
+
+    $('main').after('<footer>This is a footer</footer>')
+
+    console.log($.html());
+});
