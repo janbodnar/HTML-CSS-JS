ac216eb523199ef8e69888f1847a2ba8a824c813
Jan Bodnar <jan.bodnar@gmail.com>
2018-07-31 19:13:15 +0200

    Create data_attributes.js

---
+++ javascript/libs/cheerio/data_attributes.js
@@ -0,0 +1,17 @@
+const cheerio = require('cheerio');
+const request = require('request');
+
+request({
+    method: 'GET',
+    url: 'http://localhost:8000'
+}, function(err, res, body) {
+
+    if (err) return console.error(err);
+
+    let $ = cheerio.load(body);
+
+    let fpEl = $('h1 + p');
+    let attrs = fpEl.attr();
+
+    console.log(attrs);
+});
