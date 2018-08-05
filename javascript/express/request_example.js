9f440a762819416a4e90cc07ba769f275d6e2aea
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-05 22:30:52 +0200

    Update request_example.js

--- javascript/express/request_example.js
+++ javascript/express/request_example.js
@@ -15,9 +15,9 @@ app.use((req, res, next) => {
 app.get("/", (req, res) => {
 
     res.send("Hello there");
-})
+});
 
 app.listen(3000, () => {
 
     console.log("Application started on port 3000");
-})
+});
