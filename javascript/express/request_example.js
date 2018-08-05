96efe852a8977c39fec99671b9537c3307946683
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-05 22:30:04 +0200

    Create request_example.js

---
+++ javascript/express/request_example.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+
+    console.log(`HTTP method: ${req.method}`);
+    console.log(`URL: ${req.url}`);
+    console.log(`IP: ${req.ip}`);
+    console.log(`Hostname: ${req.hostname}`);
+    console.log(`Protocol: ${req.protocol}`);
+    next();
+});
+
+app.get("/", (req, res) => {
+
+    res.send("Hello there");
+})
+
+app.listen(3000, () => {
+
+    console.log("Application started on port 3000");
+})
