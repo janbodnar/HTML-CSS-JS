345415d4f7d2fe71fe15f77a986533c1921c03b8
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-20 16:53:13 +0200

    Create character_class2.js

---
+++ javascript/regex/character_class2.js
@@ -0,0 +1,7 @@
+console.log(/[0123456789]+/.test('She was born in 1992'));
+console.log(/[0-9]+/.test('She was born in 1992'));
+
+console.log('*******************');
+
+console.log(/[^0-9]+/.test('forest'));
+console.log(/[a-zA-z]+/.test('Sun'));
