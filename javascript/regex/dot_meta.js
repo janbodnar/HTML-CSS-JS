2ca32797b71e1fe3d318d97a2a908e0346c1ec4f
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-20 16:53:51 +0200

    Create dot_meta.js

---
+++ javascript/regex/dot_meta.js
@@ -0,0 +1,12 @@
+let words = ['seven', 'even', 'prevent', 'revenge', 'maven', 
+    'eleven', 'amen', 'event'];
+
+let pattern = /..even/;
+
+words.forEach(word => {
+
+    if (pattern.test(word)) {
+
+        console.log(`the ${word} matches`);
+    }
+});
