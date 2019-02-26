8f323394dc0fa61b4341f70c43959f97caac01a3
Jan Bodnar <jan.bodnar@gmail.com>
2019-02-26 13:01:28 +0100

    Create property_dotnotation.js

---
+++ javascript/objects/property_dotnotation.js
@@ -0,0 +1,13 @@
+const bag = {
+    colour: 'green',
+    size: 'medium',
+    items: {
+        'coins': 4,
+        'books': 2, 
+        'glasses': 1
+    }
+}
+
+// accessing object properties with dot operator
+console.log(bag.colour); 
+console.log(bag.items.coins);
