d4639dce028251ba53c713014d58b29a1fff519b
Jan Bodnar <jan.bodnar@gmail.com>
2019-02-26 13:10:48 +0100

    Create method_shorthands.js

---
+++ javascript/objects/method_shorthands.js
@@ -0,0 +1,22 @@
+// ECMAScript 2015 introduced a shorter syntax for method definitions 
+// info() {}  or info: function() {}
+
+const book = {
+    title: 'The Chase of the Golden Meteor',
+    author: 'Jules Verne',
+    info() {
+
+        return `${this.author}: ${this.title}`;
+    }
+}
+
+
+
+// const book = {
+//     title: 'The Chase of the Golden Meteor',
+//     author: 'Jules Verne',
+//     info: function() {
+
+//         return `${this.author}: ${this.title}`;
+//     }
+// }
