d84258140a1fa4584c5fdee8bfcea966ecc08716
Jan Bodnar <jan.bodnar@gmail.com>
2019-02-26 12:56:38 +0100

    Create shorthand_property.js

---
+++ javascript/objects/shorthand_property.js
@@ -0,0 +1,6 @@
+const activity = 'The Chase of the Golden Meteor';
+const author = 'Jules Verne';
+
+// createing object with shorthand property notation
+const book = { author, activity };
+console.log(book); 
