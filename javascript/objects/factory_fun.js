27065afee340f1e92f440a5294dd78569f09afde
Jan Bodnar <jan.bodnar@gmail.com>
2019-02-26 13:19:38 +0100

    Create factory_fun.js

---
+++ javascript/objects/factory_fun.js
@@ -0,0 +1,17 @@
+// A factory function produces objects
+ 
+const dogFactory = (name, age, breed) => {
+    return {
+        name: name,
+        age: age,
+        breed: breed,
+        bark() {
+            return 'Woof!'
+        }
+    };
+};
+
+let dog = dogFactory('max', 4, 'dalmatian');
+
+console.log(dog.bark());
+console.log(dog);
